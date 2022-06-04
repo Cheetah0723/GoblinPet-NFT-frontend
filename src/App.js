3c66a08c2e5819d363f7888e094a2323eb330063
Cheetah0723 <iyuki723@gmail.com>
2022-06-04 23:15:09 +0900

    complete frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="contacts">
+          <a href="#">
+            <img src="img/twitter.png" alt="twitter" />
+          </a>
+          <a href="#">
+            <img src="img/opensea.png" alt="opensea" />
+          </a>
+        </div>
+        <p className="title">Goblinpets</p>
+        <div className="flex">
+          <div className="phone">
+            <div className="phone-main">
+              <div className="phone-content">
+                <p>uaahrgg</p>
+                <p>mint</p>
+                <p>arghhhuma</p>
+                <p>here</p>
+              </div>
+              <div>
+                <div className="mint-amount">
+                  <div className="mint-select flex">
+                    <button>-</button>
+                    <p>0</p>
+                    <button>+</button>
+                  </div>
+                  <div className="mint">
+                    <p>Mint</p>
+                  </div>
+                </div>
+                <p className="mint-supply">0/5000</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="content">
+              <p>
+                <span>5 free mints +</span> gas fee per wallet. Don't be greedy
+                that's why we are here.
+                <hr />
+                <span>
+                  We are reserving 500 goblin pets.
+                  <br />
+                </span>{" "}
+                Because we want to.
+                <hr />
+                No roadmap. No Discord. No utility. CCO. <br />
+                <a href="#">Contact</a> wasn't actually written by goblin pets.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="images">
+          <div className="apes">
+            <img src="img/Goblinpet1.png" alt="ape1" id="ape1" />
+            <img src="img/Goblinpet2.png" alt="ape2" id="ape2" />
+            <img src="img/Goblinpet3.png" alt="ape3" id="ape3" />
+            <img src="img/Goblinpet4.png" alt="ape4" id="ape4" />
+          </div>
+          <img src="img/land.png" className="land" alt="land" />
+        </div>
+      </div>
     </div>
   );
 }
